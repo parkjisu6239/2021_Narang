773db7bf12e0473cfcba6680ce30cc0250c7ae09
김준환 <vkffkehlss@gmail.com>
2021-07-23 14:37:04 +0900

    [BE/jun] 채팅 서버 구현

---
+++ back/src/main/java/com/exp/narang/chat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.exp.narang.chat.controller;
+
+import com.exp.narang.chat.model.ChatModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController {
+    private final SimpMessagingTemplate template;
+    // /receive를 메시지를 받을 endpoint로 설정합니다.
+    @MessageMapping("/server")
+    // 1) /server에서 메시지를 받고, /client로 메시지를 보내줍니다.
+    public void sendMessage(ChatModel chatModel) {
+        Long roomId = chatModel.getRoomId();
+        // /client/roomId 로 메시지를 반환합니다. 프론트에서 구독한 endpoint 경로이름
+        template.convertAndSend("/client/" + roomId, chatModel);
+    }
+}
