afa95d00dea0405a2b105c1591f6f635a7ba5f94
김준환 <vkffkehlss@gmail.com>
2021-07-23 15:40:52 +0900

    [BE/jun] 채팅 서버 endpoint 수정

--- back/src/main/java/com/exp/narang/config/WebSocketConfig.java
+++ back/src/main/java/com/exp/narang/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
 }
 
