8cf58dacb8ccf55b386f2c380fbd64f5f8ca6857
email <tlsdh1111@gmail.com>
2021-07-23 17:22:22 +0900

    [FE/dong] game-room | Create Game Room Components && Skeleton inside Room

---
+++ front/src/views/game-room/game-room-setting/game-room-info-change-dialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-dialog
+    title="방 설정"
+    v-model="state.dialogVisible"
+    @close="handleClose">
+    <span style="margin-right: 10px;">제목</span>
+    <el-input placeholder="변경할 제목" v-model="state.roomTitle"></el-input>
+
+    <span style="margin-right: 10px;">최대인원</span>
+    <el-select v-model="state.value" filterable placeholder="Select">
+      <el-option
+        v-for="item in state.options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+
+  </el-dialog>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
+export default {
+  name: 'GameRoomInfoChangeDialog',
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    const state = reactive({
+      dialogVisible: computed(() => props.open),
+      value: '',
+      roomTitle: '',
+      options: [
+        {value: 4, label: '4명'},
+        {value: 5, label: '5명'},
+        {value: 6, label: '6명'},
+        {value: 7, label: '7명'},
+        {value: 8, label: '8명'},
+        {value: 9, label: '9명'},
+      ]
+
+    })
+
+    const handleClose = () => {
+      emit('closeDialog')
+    }
+
+    return { state, handleClose }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/views/game-room/game-room-setting/game-room-setting.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="setting-container">
+    <div class="game-btn">
+      <el-button type="primary">gamestart</el-button>
+      <el-button>마피아 게임</el-button>
+      <el-button>콜마이네임</el-button>
+    </div>
+    <div class="setting-btn">
+      <el-button>소리끄기</el-button>
+      <el-button>화면끄기</el-button>
+      <el-button @click="openDialog">방 정보 수정</el-button>
+    </div>
+  </div>
+</template>
+<style>
+  @import url('./game-room-setting.css');
+</style>
+<script>
+export default {
+  name: 'GameRoomSetting',
+  setup(props, { emit }) {
+
+    const openDialog = () => {
+      emit('openDialog')
+    }
+
+    return { openDialog }
+  }
+}
+</script>
+
+
