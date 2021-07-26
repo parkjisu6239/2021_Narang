68c80568e33160d69c0f21536c9c6797ad988486
yeeybook <yeeybook@gmail.com>
2021-07-26 16:33:57 +0900

    [BE/ye] room create, delete, enter | 비밀번호 추가, 방장 유무에 따라 달라지는 삭제와 입장

--- back/src/main/java/com/exp/narang/api/request/RoomReadGetReq.java
+++ back/src/main/java/com/exp/narang/api/request/RoomReadGetReq.java
@@ -6,10 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@ApiModel("RoomRegisterPostRequest")
-public class RoomRegisterPostReq {
-    String title;
-    Long ownerId;
-    int maxPlayer;
+@ApiModel("RoomReadGetRequest")
+public class RoomReadGetReq {
     int password;
 }
