4fe9168ab13d1821cd11943405a4b63413a58f29
김담영 <damyoung1225@gmail.com>
2021-07-27 22:14:39 +0900

    <BE/dam> room sort, isActivate default | 정렬 조건 추가, 방 생성시 isActivate 컬럼 default value true 설정

--- back/src/main/java/com/exp/narang/db/entity/Room.java
+++ back/src/main/java/com/exp/narang/db/entity/Room.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -29,6 +30,7 @@ public class Room {
     String thumbnailUrl;
     String title;
     String game;
+    @ColumnDefault("True")
     Boolean isActivate;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
