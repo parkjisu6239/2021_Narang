dc93b6363b5c12023b55a7d05b622fb8f5ecea75
김준환 <vkffkehlss@gmail.com>
2021-07-29 10:42:29 +0900

    [BE/ jun] 방 삭제하면 방장 위임하도록 수정

--- back/src/main/java/com/exp/narang/api/service/RoomServiceImpl.java
+++ back/src/main/java/com/exp/narang/api/service/RoomServiceImpl.java
@@ -8,6 +8,7 @@ import com.exp.narang.db.entity.User;
 import com.exp.narang.db.repository.RoomRepository;
 import com.exp.narang.db.repository.RoomRepositorySupport;
 import com.exp.narang.db.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class RoomServiceImpl implements RoomService {
     @Autowired
     RoomRepository roomRepository;
@@ -56,17 +58,24 @@ public class RoomServiceImpl implements RoomService {
 
     @Override
     public void deleteRoom(Room room, User user) {
+        log.error("이것이 방번호 다 : "+room.getRoomId());
         user.setRoom(null); // 방 나가면 유저 테이블에서도 방 정보 삭제
         userRepository.save(user);
-        if(user.getUserId() == room.getOwnerId()){ // 방 나가는 사람이 방장이라면 방 자체를 삭제
-            System.out.println("방번호 : "+room.getRoomId());
-            List<User> userList = findUserListByRoomId(room.getRoomId());
-            for(User users : userList){
-                users.setRoom(null); // 방 나가면 유저 테이블에서도 방 정보 삭제
-                userRepository.save(user);
-            }
+
+        List<User> userList = findUserListByRoomId(room.getRoomId());
+        if(!room.getOwnerId().equals(user.getUserId())) return;
+
+        userList.remove(user);
+        // 방에 1명만 있으면 방 삭제
+        if(userList.size() == 1) {
             roomRepository.deleteById(room.getRoomId());
+            return;
         }
+
+        // 방 나가는 사람이 방장이라면 방장 위임
+        User u = userList.get(0);
+        room.setOwnerId(u.getUserId());
+        roomRepository.save(room);
     }
 
     @Override
