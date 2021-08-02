1dda743340ac4fc97823e7012e7259fb358562fe
yeeybook <yeeybook@gmail.com>
2021-08-02 18:02:50 +0900

    [FE/ye] Tensorflow.js 적용 중

--- front/src/views/game-room/game-room-webcam/game-room-webcam.vue
+++ front/src/views/game-room/game-room-webcam/game-room-webcam.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="webcam-container">
+    <button type="button" @click="init()">Start</button>
+<div><canvas id="canvas"></canvas></div>
     <div id="video-container" class="col-md-6">
 				<user-video :stream-manager="state.publisher" @click="updateMainVideoStreamManager(state.publisher)"/>
 				<user-video v-for="sub in state.subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" @click="updateMainVideoStreamManager(sub)"/>
@@ -9,11 +11,14 @@
 <style scoped>
   @import url('game-room-webcam.css');
 </style>
+
 <script>
 import $axios from 'axios'
 import { computed, reactive } from 'vue'
 import { OpenVidu } from 'openvidu-browser'
 import UserVideo from './components/UserVideo'
+import '@tensorflow/tfjs';
+import * as tmPose from "@teachablemachine/pose";
 
 $axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default {
@@ -39,6 +44,76 @@ export default {
 			mySessionId: computed(() => props.roomId),
 			myUserName: 'Participant' + Math.floor(Math.random() * 100),
     })
+
+ // More API functions here:
+    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+
+    // the link to your model provided by Teachable Machine export panel
+    const URL = "./pose-model/";
+    let model, webcam, ctx, labelContainer, maxPredictions;
+
+    const init = () =>  {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const size = 200;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = size; canvas.height = size;
+        ctx = canvas.getContext("2d");
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop(timestamp) {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    async function predict() {
+        // Prediction #1: run input through posenet
+        // estimatePose can take in an image, video or canvas html element
+        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+        // Prediction 2: run input through teachable machine classification model
+        const prediction = await model.predict(posenetOutput);
+
+        for (let i = 0; i < maxPredictions; i++) {
+            const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+
+        // finally draw the poses
+        drawPose(pose);
+    }
+
+    function drawPose(pose) {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
 
     const joinSession = () => {
       console.log('조인세션 시작 중')
