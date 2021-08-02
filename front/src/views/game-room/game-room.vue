0697d02d61dcab4c05e52d48698fbe96f111ed55
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-02 16:41:02 +0900

    [FE/jisu] UIUX mafia day/night change 10s

--- front/src/views/game-room/game-room.vue
+++ front/src/views/game-room/game-room.vue
@@ -8,6 +8,21 @@
   </article>
   <GameRoomInfoChangeDialog @closeDialog="closeDialog" :open="state.open" :roomId="route.params.roomId"/>
 
+  <div>
+    <img class="city" :src="require('@/assets/images/mafia/city.png')" alt="">
+    <img class="mafia-neorang" :src="require('@/assets/images/mafia/mafia.png')" alt="">
+    <div class="circle moon"></div>
+    <div class="night-background"></div>
+  </div>
+  <!-- <div>
+    <img class="land" :src="require('@/assets/images/mafia/land.png')" alt="">
+    <img class="cloud1" :src="require('@/assets/images/mafia/cloud1.png')" alt="">
+    <img class="cloud2" :src="require('@/assets/images/mafia/cloud2.png')" alt="">
+    <img class="cloud3" :src="require('@/assets/images/mafia/cloud3.png')" alt="">
+    <div class="circle sun"></div>
+    <div class="day-background"></div>
+  </div> -->
+
 </template>
 <style scoped>
   @import url('./game-room.css');
