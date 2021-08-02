801cc69e471e4283ccafc83ef099b79c31720e16
yeeybook <yeeybook@gmail.com>
2021-08-02 16:08:37 +0900

    [BE/ye] websocket model 수정

--- back/src/main/java/com/exp/narang/websocket/controller/CallmyController.java
+++ back/src/main/java/com/exp/narang/websocket/controller/CallmyController.java
@@ -15,12 +15,12 @@ public class CallmyController {
     // @EnableWebSocketMessageBroker를 통해서 등록되는 bean
     private final SimpMessagingTemplate template;
     // /server 메시지를 받을 endpoint로 설정합니다.
-    @MessageMapping("/server")
+    @MessageMapping("callmy/server")
     // 1) /server에서 메시지를 받고, /client로 메시지를 보내줍니다.
     public void sendMessage(CallmyModel callmyModel) {
         Long roomId = callmyModel.getRoomId();
         log.debug(callmyModel.toString());
         // /client/roomId 로 메시지를 반환합니다. 프론트에서 구독한 endpoint 경로이름
-        template.convertAndSend("/client/" + roomId, callmyModel);
+        template.convertAndSend("/client" + roomId, callmyModel);
     }
 }
