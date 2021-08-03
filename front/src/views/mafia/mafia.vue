12fcfdcf1e1f9a0fe4fe58c862867f0049d500b8
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-03 16:12:11 +0900

    [FE/jisu] mafia 컴포넌트에 배경 적용, 기존 게임룸 컴포넌트에는 제거

--- front/src/views/mafia/mafia.vue
+++ front/src/views/mafia/mafia.vue
@@ -1,31 +1,45 @@
 <template>
   <h1>마피아 게임 방</h1>
-  <el-radio-group v-model="radio1">
-    <el-radio-button label="New York"></el-radio-button>
-    <el-radio-button label="Washington"></el-radio-button>
-    <el-radio-button label="Los Angeles"></el-radio-button>
-    <el-radio-button label="Chicago"></el-radio-button>
+  <el-radio-group v-model="state.radio">
+    <el-radio-button label="day"></el-radio-button>
+    <el-radio-button label="night"></el-radio-button>
   </el-radio-group>
 
-  <div>
+  <div v-if="state.radio === 'night'">
     <img class="city" :src="require('@/assets/images/mafia/city.png')" alt="">
     <img class="mafia-neorang" :src="require('@/assets/images/mafia/mafia.png')" alt="">
     <div class="circle moon"></div>
     <div class="night-background"></div>
   </div>
-  <!-- <div>
+  <div v-else>
     <img class="land" :src="require('@/assets/images/mafia/land.png')" alt="">
     <img class="cloud1" :src="require('@/assets/images/mafia/cloud1.png')" alt="">
     <img class="cloud2" :src="require('@/assets/images/mafia/cloud2.png')" alt="">
     <img class="cloud3" :src="require('@/assets/images/mafia/cloud3.png')" alt="">
     <div class="circle sun"></div>
     <div class="day-background"></div>
-  </div> -->
+  </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+
 export default {
   name: 'mafia',
+
+  setup(props, { emit }) {
+    const store = useStore()
+    const route = useRoute()
+    const router = useRouter()
+
+    const state = reactive({
+      radio: ref('day'),
+    })
+
+    return { state }
+  }
 
 }
 </script>
