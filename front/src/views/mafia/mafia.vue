50621d3a75798f52a82622ca0c21d06ee1aa62ee
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-03 16:01:08 +0900

    [FE/jisu] 방장이 게임 시작 누르면 54321

--- front/src/views/mafia/mafia.vue
+++ front/src/views/mafia/mafia.vue
@@ -1,13 +1,35 @@
 <template>
   <h1>마피아 게임 방</h1>
+  <el-radio-group v-model="radio1">
+    <el-radio-button label="New York"></el-radio-button>
+    <el-radio-button label="Washington"></el-radio-button>
+    <el-radio-button label="Los Angeles"></el-radio-button>
+    <el-radio-button label="Chicago"></el-radio-button>
+  </el-radio-group>
+
+  <div>
+    <img class="city" :src="require('@/assets/images/mafia/city.png')" alt="">
+    <img class="mafia-neorang" :src="require('@/assets/images/mafia/mafia.png')" alt="">
+    <div class="circle moon"></div>
+    <div class="night-background"></div>
+  </div>
+  <!-- <div>
+    <img class="land" :src="require('@/assets/images/mafia/land.png')" alt="">
+    <img class="cloud1" :src="require('@/assets/images/mafia/cloud1.png')" alt="">
+    <img class="cloud2" :src="require('@/assets/images/mafia/cloud2.png')" alt="">
+    <img class="cloud3" :src="require('@/assets/images/mafia/cloud3.png')" alt="">
+    <div class="circle sun"></div>
+    <div class="day-background"></div>
+  </div> -->
 </template>
 
 <script>
 export default {
+  name: 'mafia',
 
 }
 </script>
 
-<style>
-
+<style scoped>
+  @import url('./mafia.css');
 </style>
