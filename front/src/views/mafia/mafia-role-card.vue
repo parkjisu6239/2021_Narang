a3aad6bb1d627e747be8a67d96b12d2ba7361df0
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-05 20:12:26 +0900

    [FE/jisu] mafia role card hover rotate animation

--- front/src/views/mafia/mafia-role-card.vue
+++ front/src/views/mafia/mafia-role-card.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="role-card">
-    <img v-if="myRole === 'Citizen'" class="role-img role-citizen" :src="require('@/assets/images/mafia/role-citizen.png')" alt="시민">
-    <img v-else class="role-img role-mafia" :src="require('@/assets/images/mafia/role-citizen.png')" alt="마피아">
-    <div class="role">{{ myRole }}</div>
+    <div class="role-card-front">
+      <img v-if="myRole === 'Citizen'" class="role-img role-citizen" :src="require('@/assets/images/mafia/role-citizen.png')" alt="시민">
+      <img v-else class="role-img role-mafia" :src="require('@/assets/images/mafia/role-mafia.png')" alt="마피아">
+      <div class="role">{{ myRole }}</div>
+    </div>
+    <div class="role-card-back">
+      <div class="card-inner-line">
+        <img class="neurang" :src="require('@/assets/images/Neurang.png')" alt="너랑">
+        <p>당신의 역할은?</p>
+        <p>마우스를 올려보세요</p>
+        <img class="neurang-mirror" :src="require('@/assets/images/Neurang.png')" alt="너랑">
+      </div>
+    </div>
   </div>
 </template>
 
