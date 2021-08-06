c09ee7c967330c5e2f3cf1949ae1efc3c3c282b4
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-07 03:54:38 +0900

    [FE/jisu] ADD | 웹 캠 관련 컴포넌트 추가, 반응형으로 구성, 최대최소 크기 지정, 중앙 정렬, 스크롤

---
+++ front/src/views/mafia/left-side/game-room-webcam/components/OvVideo.vue
@@ -0,0 +1,19 @@
+<template >
+  <video class="webcam" autoplay playsinline controls="false"/>
+</template>
+
+<script>
+export default {
+  name: 'OvVideo',
+  props: {
+    streamManager: Object,
+  },
+  mounted() {
+    this.streamManager.addVideoElement(this.$el);
+  }
+}
+</script>
+<style scoped>
+  @import url('OvVideo.css');
+</style>
+
