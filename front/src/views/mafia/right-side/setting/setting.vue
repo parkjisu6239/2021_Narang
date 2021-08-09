b51a378bf9313efe1e6cda2c5c827b3636808693
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-09 11:31:28 +0900

    [FE/jisu] 버튼 추가, 타이머 추가 | 컴포넌트 추가

---
+++ front/src/views/mafia/right-side/setting/setting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>음소거</button>
+    <button>카메라끄기</button>
+    <button>나가기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'setting'
+}
+</script>
+
+<style>
+ @import url('./setting.css');
+</style>
