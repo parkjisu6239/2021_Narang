d2783066fc042888efbbbd51a8abfa17c6c76202
김준환 <vkffkehlss@gmail.com>
2021-08-10 12:22:20 +0900

    [BE/jun] 질문 순서, 순위 메서드 추가

--- back/src/main/java/com/exp/narang/websocket/callmyname/controller/CallMyNameController.java
+++ back/src/main/java/com/exp/narang/websocket/callmyname/controller/CallMyNameController.java
@@ -11,6 +11,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -51,20 +52,41 @@ public class CallMyNameController {
     }
 
     /**
+     * 다음 질문 순서 userId를 반환하는 메서드
+     * TODO : 순서대로 되는 로직인지 확인하기
+     * @param roomId : path로 받는 roomId (PK)
+     * @return 다음에 질문할 사용자의 userId
+     */
+    @MessageMapping("/call/set-qtime/{roomId}")
+    @SendTo("/from/call/set-qtime/{roomId}")
+    public long setQuestionTime(@DestinationVariable long roomId){
+        return ManagerHolder.gameManagerMap.get(roomId).getNextUserId();
+    }
+
+    /**
      * 사용자가 자신의 이름을 맞힐 때 호출되는 메서드
      * TODO : 반환값 필요한거 추가하기
      * @param roomId : path로 받는 roomId (PK)
      * @param req : userId와 정해진 이름이 있는 객체
-     * @return 다음 턴 사용자의 userId, 답이 맞았는지, 게임이 끝났는지 여부를 멤버변수로 가진 객체
+     * @return 답이 맞았는지, 게임이 끝났는지 여부를 멤버변수로 가진 객체
      */
     @MessageMapping("/call/guess-name/{roomId}")
     @SendTo("/from/call/guess-name/{roomId}")
     public GuessNameRes guessName(@DestinationVariable long roomId, NameReq req){
-        GuessNameRes guessNameRes = ManagerHolder.gameManagerMap.get(roomId).guess(req);
+        GuessNameRes guessNameRes = ManagerHolder.gameManagerMap.get(roomId).guessName(req);
         // 게임 끝나면 Map에서 Manager 삭제
         if(guessNameRes.isGameEnd()) ManagerHolder.gameManagerMap.remove(roomId);
         return guessNameRes;
     }
 
-
+    /**
+     * 정답 순서를 반환하는 메서드
+     * @param roomId
+     * @return 정답을 맞힌 순서대로 userId가 담긴 리스트
+     */
+    @MessageMapping("/call/get-rank/{roomId}")
+    @SendTo("/from/call/get-rank/{roomId}")
+    public List<Long> getRank(@DestinationVariable long roomId){
+        return ManagerHolder.gameManagerMap.get(roomId).getRank();
+    }
 }
