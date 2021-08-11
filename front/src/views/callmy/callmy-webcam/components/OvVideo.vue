f1f195af3b701730c90521d31a9908a27a5ddbbe
email <tlsdh1111@gmail.com>
2021-08-11 14:13:56 +0900

    [FE/dong] call my cam

---
+++ front/src/views/callmy/callmy-webcam/components/OvVideo.vue
@@ -0,0 +1,25 @@
+<template>
+  <video class="webcam" autoplay playsinline controls="false"/>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+
+export default {
+  name: 'OvVideo',
+  props: {
+    streamManager: Object,
+  },
+  setup(props, {emit}) {
+    const myWebCam = ref(null)
+
+    onMounted(() => {
+      props.streamManager.addVideoElement(myWebCam.value)
+    })
+  }
+}
+</script>
+<style scoped>
+  @import url('OvVideo.css');
+</style>
+
