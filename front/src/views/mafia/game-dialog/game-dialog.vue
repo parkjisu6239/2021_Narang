38cb2ccd75b4c1308c0c866f2d02178802f3bf50
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-11 16:31:38 +0900

    [FE/jisu] mafia modal, 게임 종료후 게임룸으로 이동

---
+++ front/src/views/mafia/game-dialog/game-dialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dialong-body">
+    <div class="dialog-container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gameDialog',
+
+  props: {
+
+  }
+}
+</script>
+
+<style>
+  @import url('./game-dialog.css');
+</style>
