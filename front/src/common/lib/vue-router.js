08ab7df0b88a3de1c1dc2b0087c30dc67010c80e
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-13 11:26:20 +0900

    [FE/jisu] router guard | 미로그인시 홈만 이동 가능, 게임룸은 대기실과 각 게임에서만 접근 가능, 마피아는 게임룸에서만 접근 가능

--- front/src/common/lib/vue-router.js
+++ front/src/common/lib/vue-router.js
@@ -1,8 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { ElMessage } from 'element-plus'
+
 import home from '@/views/home/home'
 import waitingRoom from '@/views/waiting-room/waiting-room'
 import gameRoom from '@/views/game-room/game-room'
 import Mypage from '@/views/mypage/mypage'
+
+
+/* navigation guard beforeEnter func */
+const requireMafia = () => (to, from, next) => {
+  if (from.name !== 'gameRoom') {
+    ElMessage({
+      type: 'error',
+      message: '접근이 불가능 합니다.'
+    })
+    return next(from)
+  } else {
+    return next()
+  }
+}
+
+const requireGameRoom = () => (to, from, next) => {
+  if (from.name !== 'waitingRoom' && from.name !== 'mafia' && from.name !== 'callmy') {
+    ElMessage({
+      type: 'error',
+      message: '접근이 불가능 합니다.'
+    })
+    return next(from)
+  } else {
+    return next()
+  }
+}
 
 function makeRoutesFromMenu() {
   let routes = []
@@ -27,12 +55,14 @@ function makeRoutesFromMenu() {
   {
     path: '/game-room/:roomId',
     name: 'gameRoom',
-    component: gameRoom
+    component: gameRoom,
+    beforeEnter: requireGameRoom()
   },
   {
     path: '/game-room/:roomId/mafia',
     name: 'mafia',
     component: () => import('@/views/mafia/mafia.vue'),
+    beforeEnter: requireMafia()
   },
   {
     path: '/game-room/:roomId/callmy',
@@ -53,5 +83,25 @@ const router = createRouter({
 router.afterEach((to) => {
   console.log(to)
 })
+
+router.beforeEach((to, from) => {
+  if (to.name !== 'home') {
+    if (!isLogedin()) {
+      ElMessage({
+        type: 'error',
+        message: '미로그인 사용자는 접근할 수 없습니다.'
+      })
+      return '/'
+    } else {
+      return true
+    }
+  }
+
+  return true
+})
+
+const isLogedin = () => {
+  return localStorage.getItem('username') ? true : false
+}
 
 export default router
