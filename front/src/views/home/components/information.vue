7e63c14a7253e42f242eb716a6a8273e96eac956
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-14 22:54:03 +0900

    [FE/jisu] UXUI | 네비게이션 수정, 홈페이지 설명 추가

--- front/src/views/home/components/information.vue
+++ front/src/views/home/components/information.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="guide">사진 설명 혹은 영상 그런 설명 들어갈 예정~~</div>
+  <div class="guide">
+    <section v-for="i in 9" :key="i">
+      <img :src="require(`@/assets/images/info/info(${i}).png`)" alt="">
+    </section>
+  </div>
 </template>
 
 <script>
@@ -10,9 +14,35 @@ export default {
 
 <style>
 .guide {
+  display: grid;
+  grid-auto-rows: repeat(auto-fill, 1fr);
+  height: 350px;
   background: rgba(255, 255, 255, 0.5);
   backdrop-filter: blur(5px);
   border-radius: 15px;
-  padding: 60px 40px;
+  box-shadow: inset 0px -3px 2px rgba(0, 0, 0, 0.025),
+              inset 0px 2px 5px rgba(0,0,0,0.15);
+  overflow: auto;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+.guide::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera*/
+}
+
+.guide section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 350px;
+  background-color: white;
+  border-bottom: 1px solid rgb(202, 202, 202);
+}
+
+.guide section img{
+  height: 100%;
+  width: auto;
 }
 </style>
