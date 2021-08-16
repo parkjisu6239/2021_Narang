319ae401d97219d046bef5f7e008b63bcece953e
email <tlsdh1111@gmail.com>
2021-08-16 15:26:33 +0900

    [FE/dong] 내 머리 위에는 ???이 뜨게 만들어줌

--- front/src/views/callmy/callmy-webcam/components/OvVideo.vue
+++ front/src/views/callmy/callmy-webcam/components/OvVideo.vue
@@ -60,7 +60,6 @@ export default {
             nickname,
           ]
 
-          console.log(state.myUserName, props.username)
           const anchor = {
             x: state.myUserName === props.username ? 600 + text[0].length * 20 - state.detections.box.topLeft.x : state.detections.box.topRight.x - text[0].length * 20,
             y: state.detections.box.topLeft.y - 30,
