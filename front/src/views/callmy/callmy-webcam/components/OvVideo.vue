a15d3839b446b0d0944e4a0df5d859cd3a45c20d
email <tlsdh1111@gmail.com>
2021-08-16 15:24:27 +0900

    [FE/dong] 내 머리 위에는 ???이 뜨게 만들어줌

--- front/src/views/callmy/callmy-webcam/components/OvVideo.vue
+++ front/src/views/callmy/callmy-webcam/components/OvVideo.vue
@@ -31,6 +31,13 @@ export default {
       height: '',
       width: '',
       myUserName: computed(() => store.state.root.username),
+      nickName: computed(() => {
+        let target
+        store.state.root.callmyManager.nowPlayUsers.forEach(user => {
+          if (user.username === username) target = username
+        })
+        return target
+      }),
       timeId: '',
     })
 
@@ -48,8 +55,9 @@ export default {
         state.detections = await faceapi.detectSingleFace(myWebCam.value, new faceapi.TinyFaceDetectorOptions())
 
         if (state.detections) {
+          const nickname = props.username === state.myUserName ? '???' : state.nickName
           const text = [
-            '아이이잉이',
+            nickname,
           ]
 
           console.log(state.myUserName, props.username)
--- front/src/views/callmy/callmy.vue
+++ front/src/views/callmy/callmy.vue
@@ -160,6 +160,7 @@ export default {
             }
           ];
           sendDefaultNickname() // 1번 사람 디폴트 닉네임 받기
+          showDraw()
         } else { // 플레이 하는 시간
           store.state.root.callmyManager.nowPlayUsers[0].nickname = result.user1.nickname
           store.state.root.callmyManager.nowPlayUsers[1].nickname = result.user2.nickname
@@ -327,13 +328,10 @@ export default {
 
 
     const showDraw = () => {
-      setTimeout(() => {
-        state.showDraw = true
-      }, 3000)
-
+      state.showDraw = true
       setTimeout(() => {
         state.showDraw = false
-      }, 7000)
+      }, 3000)
     }
 
 
