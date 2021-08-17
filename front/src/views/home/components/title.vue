3394e7b7c9015f93835c8690c677031ff1d9c06e
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-17 12:07:41 +0900

    [FE/jisu] home typing ani

--- front/src/views/home/components/title.vue
+++ front/src/views/home/components/title.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="title">
     <div class="narang">나랑</div>
-    <div class="descript">아이스브레이킹 웹 화상 게임 서비스</div>
   </div>
 </template>
 
@@ -22,7 +21,92 @@ export default {
   display: inline-block;
 }
 
-.title .descript{
-  display: inline-block;
+.narang::after {
+  font-size: 16px;
+  font-weight: 200;
+  content: '';
+  animation: typing 5s 5s infinite;
 }
+
+@keyframes typing {
+  0% {
+    content: '';
+  }
+  4% {
+    content: '|';
+  }
+  8% {
+    content: '';
+  }
+  12% {
+    content: '아|';
+  }
+  16% {
+    content: '아이|';
+  }
+  20% {
+    content: '아이스|';
+  }
+  24% {
+    content: '아이스브|';
+  }
+  28% {
+    content: '아이스브레|';
+  }
+  32% {
+    content: '아이스브레|';
+  }
+  36% {
+    content: '아이스브레이|';
+  }
+  40% {
+    content: '아이스브레이킹|';
+  }
+  44% {
+    content: '아이스브레이킹 |';
+  }
+  48% {
+    content: '아이스브레이킹 웹|';
+  }
+  52% {
+    content: '아이스브레이킹 웹 |';
+  }
+  56% {
+    content: '아이스브레이킹 웹 화|';
+  }
+  60% {
+    content: '아이스브레이킹 웹 화상|';
+  }
+  64% {
+    content: '아이스브레이킹 웹 화상 |';
+  }
+  68% {
+    content: '아이스브레이킹 웹 화상 게|';
+  }
+  72% {
+    content: '아이스브레이킹 웹 화상 게임|';
+  }
+  76% {
+    content: '아이스브레이킹 웹 화상 게임 |';
+  }
+  80% {
+    content: '아이스브레이킹 웹 화상 게임 서|';
+  }
+  84% {
+    content: '아이스브레이킹 웹 화상 게임 서비|';
+  }
+  88% {
+    content: '아이스브레이킹 웹 화상 게임 서비스|';
+  }
+  92% {
+    content: '아이스브레이킹 웹 화상 게임 서비스';
+  }
+  96% {
+    content: '아이스브레이킹 웹 화상 게임 서비스|';
+  }
+  100% {
+    content: '아이스브레이킹 웹 화상 게임 서비스';
+  }
+}
+
 </style>
