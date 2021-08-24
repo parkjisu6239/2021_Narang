eea6cc8ac7144095f122a4591b7450917c41091c
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-24 20:37:22 +0900

    [FE/jisu] ele plus 오류 lib 제거

--- front/src/views/main/main.vue
+++ front/src/views/main/main.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 <style>
-  @import "https://unpkg.com/element-plus/lib/theme-chalk/index.css";
+  @import "https://unpkg.com/element-plus/theme-chalk/index.css";
   @import './main.css';
   @import '../../common/css/common.css';
   @import '../../common/css/element-plus.css';
