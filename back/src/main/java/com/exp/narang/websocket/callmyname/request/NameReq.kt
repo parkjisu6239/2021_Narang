bf11c90c5e8ef4258f1e06fafcdffe1bb93a293a
김준환 <vkffkehlss@gmail.com>
2021-11-21 22:11:51 +0900

    refactor: Refactor codes

--- back/src/main/java/com/exp/narang/websocket/callmyname/request/NameReq.kt
+++
@@ -1,3 +0,0 @@
-package com.exp.narang.websocket.callmyname.request
-
-class NameReq (val userId: Long, val name: String)
--- back/src/main/java/com/exp/narang/websocket/callmyname/request/SetNameReq.kt
+++
@@ -1,8 +0,0 @@
-package com.exp.narang.websocket.callmyname.request
-
-class SetNameReq (val userId: Long, // 투표자의 userId (send 요청을 보낸 장본인)
-                  val targetId: Long, // 타겟의 userId
-                  val content: String, // 제시어
-                  val vote: Int, // 투표 추가 시 1, 투표 철회 시 -1, 제시어 입력 시 0
-                  val isFinished: Boolean // 제한시간 끝나면 true, 아니면 false
-                  )
